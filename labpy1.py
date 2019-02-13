ea8910275463f20248e153f7e4d600822c403a19
“Rahman” <rh35235@gmail.com>
2019-02-13 16:34:16 +0700

    tugas

---
+++ labpy1.py
@@ -0,0 +1,12 @@
+print ("mencai bilangan terbesar dari 3 bilangan")
+
+a = int(input("masukan bilangan pertama :"))
+b = int(input("masukan bilangan kedua :"))
+c = int(input("masukan bilangan ketiga :"))
+
+if a > b and a > c :
+        print ("\nBilangan terbesar nya adalah :%s " %a)
+elif b > a and b > c :
+        print ("\nBilingan terbesar nya adalah :%s " % b)
+else :
+        print ("\nBilangan terbesar nya adalah :%s " % c)
